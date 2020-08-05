63d9a893fbccccfd15ab0b2aa0e3c009c7fdcdd6
Reno-ID <69244302+Reno-ID@users.noreply.github.com>
2020-08-05 18:42:31 +0700

    My_Tools

---
+++ All-Tools.sh
@@ -0,0 +1,32 @@
+# My_Toolsfiglet
+            "My_Tools"
+echo "======================================="
+echo "Nickname  :Reno_ID
+echo "Instagram :renoprayudha_
+echo "GitHub    :https://github.com/Reno_ID
+echo "======================================="
+echo
+echo "[+] PILIH MENU TOOLS [+]"
+echo
+echo "[1] Spammer"
+echo "[2] Install bahan"
+
+read -p "[?] Pilih : "pil;
+
+if [ $pil = 1 ]
+then
+git clone https://github.com/Zidan-ID/Spam-Tools
+cd Spam-Tools
+python2 umbrella.py
+fi
+
+if [ $pil = 2 ]
+apt update && apt upgrade
+pkg install nano
+pkg install python
+pkg install git
+pkg install python2
+pip install requests mechanize
+pip2 install requests mechanize
+pkg install php
+fi
